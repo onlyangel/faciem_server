12607f933a418cfbbbdb42842de86e04e1af40e1
Jose Angel Espinoza Portillo <jose@ngel.es>
2016-07-02 19:35:43 -0500

    first test of the remote evaluator server.

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"net/http"
 	"fmt"
+	"time"
+	"os"
+	"io"
+	"os/exec"
 )
 
 func main(){
-	http.HandleFunc("/", handler)
+	//http.HandleFunc("/", handler)
 	http.HandleFunc("/home", uploadRoot)
 	http.HandleFunc("/evaluate",evaluatehandler)
 	http.ListenAndServe(":8080", nil)
@@ -16,14 +20,55 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func uploadRoot(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, `
+	fmt.Fprintf(w, `<html><body>
 	<form action="/evaluate" method="POST" enctype='multipart/form-data'>
 	primer <input type="file" name="f1"><br>
-	segundo <input type="file" name="f2><br>
+	segundo <input type="file" name="f2"><br>
 	<input type="submit" value="Carga">
-	</form>
+	</form></body></html>
 	`)
 }
 func evaluatehandler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "uploaded =D")
+
+	r.ParseMultipartForm(32 << 20)
+
+	f1,err := parseFile(r,"f1")
+	if err != nil{
+		fmt.Fprintf(w,"ERROR: %v",err)
+		return
+	}
+	f2,err := parseFile(r,"f2")
+	if err != nil{
+		fmt.Fprintf(w,"ERROR: %v",err)
+		return
+	}
+
+	fmt.Printf("Archivos '%s', '%s'",f1,f2)
+
+	out, err := exec.Command("br","-algorithm","FaceRecognition","-compare",f1,f2).Output()
+	if err != nil {
+		fmt.Fprintf(w,"ERROR: %v",err)
+		return
+	}
+	fmt.Printf("OUTPUT '%s'\n", out)
+}
+
+func parseFile(r *http.Request, filevar string)(string,error){
+	file, handler, err := r.FormFile(filevar)
+	if err != nil {
+		return "",err
+	}
+	defer file.Close()
+	fmt.Printf("%v", handler.Header)
+	crutime := time.Now().Unix()
+	filename := fmt.Sprintf("/tmp/downloads/%d_%s",crutime,handler.Filename)
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		return "",err
+	}
+	defer f.Close()
+	io.Copy(f, file)
+
+	return filename,nil
 }
