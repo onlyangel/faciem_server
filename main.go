1253f0a788de1e7df1294c45bef45dc6d19a238e
Jose Angel Espinoza Portillo <jose@ngel.es>
2016-07-02 20:14:06 -0500

    trimming the output value

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"io"
 	"os/exec"
+	"strings"
 )
 
 func main(){
@@ -50,6 +51,7 @@ func evaluatehandler(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w,"ERROR: %v",err)
 		return
 	}
+	out = strings.Trim(out, " \n\t")
 	fmt.Fprintf(w,"OUTPUT '%s'\n", out)
 	os.Remove(f1)
 	os.Remove(f2)
